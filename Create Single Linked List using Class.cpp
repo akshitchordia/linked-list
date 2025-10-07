ceaac3f7a90d5474a9636e30ba3810f2cacbd657
akshitchordia <chordiaakshit3@gmail.com>
2025-10-07 17:51:02 +0530

    Create Create Single Linked List using Class.cpp

---
+++ Create Single Linked List using Class.cpp
@@ -0,0 +1,26 @@
+//akshit chordia
+//24070123008
+
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int val;
+    Node*next;
+    
+    Node(int data){
+        val=data;
+        next=NULL;
+    }
+};
+
+int main() {
+    Node*n=new Node(20);
+    cout<<n->val<<" "<<n->next<<endl;
+
+    return 0;
+}
+
+//OUTPUT:
+//20 0
